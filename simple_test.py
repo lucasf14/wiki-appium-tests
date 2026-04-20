3f2766bf9895ce99746151f25c1b4441768854b5
Lucas Ferreira <lucassky14@gmail.com>
2026-04-20 16:48:20 +0100

    Refactor tess from unittest to pytest

--- simple_test.py
+++ simple_test.py
@@ -1,70 +1,39 @@
-import unittest
-from appium import webdriver
-from appium.options.android import UiAutomator2Options
 from appium.webdriver.common.appiumby import AppiumBy
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-capabilities = dict(
-    platformName="Android",
-    automationName="uiautomator2",
-    deviceName="Android",
-    appPackage="org.wikipedia",
-    appActivity=".main.MainActivity",
-    language="en",
-    locale="US",
-)
-
-appium_server_url = "http://localhost:4723"
+from selenium.webdriver.support.wait import WebDriverWait
 
 
-class TestAppium(unittest.TestCase):
-    def setUp(self) -> None:
-        self.driver = webdriver.Remote(
-            appium_server_url,
-            options=UiAutomator2Options().load_capabilities(capabilities),
+def test_wiki_search(appium_driver) -> None:
+    wait = WebDriverWait(appium_driver, 10)
+
+    skip_button = wait.until(
+        EC.element_to_be_clickable(
+            (AppiumBy.ID, "org.wikipedia:id/fragment_onboarding_skip_button")
         )
-        self.wait = WebDriverWait(self.driver, 10)
+    )
+    skip_button.click()
 
-    def tearDown(self) -> None:
-        if self.driver:
-            self.driver.quit()
+    search_container = wait.until(
+        EC.element_to_be_clickable((AppiumBy.ID, "org.wikipedia:id/search_container"))
+    )
+    search_container.click()
 
-    def test_wiki_search(self) -> None:
-        skip_button = self.wait.until(
-            EC.element_to_be_clickable(
-                (AppiumBy.ID, "org.wikipedia:id/fragment_onboarding_skip_button")
-            )
+    search_input = wait.until(
+        EC.visibility_of_element_located(
+            (AppiumBy.ID, "org.wikipedia:id/search_src_text")
         )
-        skip_button.click()
+    )
+    search_input.send_keys("Lucas")
 
-        search_container = self.wait.until(
-            EC.element_to_be_clickable(
-                (AppiumBy.ID, "org.wikipedia:id/search_container")
-            )
+    search_results = wait.until(
+        EC.visibility_of_element_located(
+            (AppiumBy.ID, "org.wikipedia:id/fragment_search_results")
         )
-        search_container.click()
+    )
 
-        search_input = self.wait.until(
-            EC.visibility_of_element_located(
-                (AppiumBy.ID, "org.wikipedia:id/search_src_text")
-            )
-        )
-        search_input.send_keys("Lucas")
+    result_elements = search_results.find_elements(
+        AppiumBy.CLASS_NAME, "android.widget.TextView"
+    )
 
-        search_results = self.wait.until(
-            EC.visibility_of_element_located(
-                (AppiumBy.ID, "org.wikipedia:id/fragment_search_results")
-            )
-        )
-
-        result_elements = search_results.find_elements(
-            AppiumBy.CLASS_NAME, "android.widget.TextView"
-        )
-
-        for element in result_elements:
-            print(f"TEXT: {element.text.strip()}")
-
-
-if __name__ == "__main__":
-    unittest.main()
+    for element in result_elements:
+        print(f"TEXT: {element.text.strip()}")
