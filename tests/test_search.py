67fc2fdc532c7ab7d8c371b752077c0572c539d2
Lucas Ferreira <lucassky14@gmail.com>
2026-04-22 11:00:09 +0100

    Move tests into 'tests' folder

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,7 +1,7 @@
 from pages.main_page import MainPage
 
 
-def test_wiki_search(appium_driver) -> None:
+def test_search(appium_driver) -> None:
     main_page = MainPage(appium_driver)
 
     main_page.click_button(main_page.skip_button)
