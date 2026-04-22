c05df1c119fec21aed769dc70bab5072bd22cf3d
Lucas Ferreira <lucassky14@gmail.com>
2026-04-22 11:19:16 +0100

    Rename page variable for clarity

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,13 +1,20 @@
 from pages.explore_page import ExplorePage
 
 
-def test_search(appium_driver) -> None:
-    main_page = ExplorePage(appium_driver)
+def test_unsuccessful_wiki_search(appium_driver) -> None:
+    explore_page = ExplorePage(appium_driver)
 
-    main_page.click_button(main_page.skip_button)
+    explore_page.click_button(explore_page.skip_button)
 
-    main_page.click_button(main_page.search_container)
+    explore_page.click_button(explore_page.search_container)
 
-    main_page.fill_input(main_page.search_input, "Lucas")
+    explore_page.fill_input(explore_page.search_input, "Invalid123")
 
-    main_page.print_results()
+    results = explore_page.find_inner_elements(
+        explore_page.search_results, explore_page.result_elements
+    )
+
+    # assert len(results) == 1
+    # assert "No results" in results[0].text.strip()
+    for result in results:
+        print(f"RESULT: {result.text.strip()}")
