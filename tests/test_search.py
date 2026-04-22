5b8535273a14fe49af63172c7c0bfe9a30cefd74
Lucas Ferreira <lucassky14@gmail.com>
2026-04-22 14:53:34 +0100

    Refactoring positive and negative tests with correct assertions

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,8 +1,8 @@
 from pages.explore_page import ExplorePage
-from data.search_data import NO_RESULTS, SEARCH_WORD_INVALID
+from data.search_data import NO_RESULTS, SEARCH_WORD_INVALID, SEARCH_WORD_VALID
 
 
-def test_unsuccessful_wiki_search(appium_driver) -> None:
+def test_unsuccessful_search(appium_driver) -> None:
     explore_page = ExplorePage(appium_driver)
 
     explore_page.click_button(explore_page.skip_button)
@@ -11,9 +11,31 @@ def test_unsuccessful_wiki_search(appium_driver) -> None:
 
     explore_page.fill_input(explore_page.search_input, SEARCH_WORD_INVALID)
 
-    results = explore_page.find_inner_elements(
+    search_results = explore_page.get_search_results(
         explore_page.search_results, explore_page.result_elements
     )
 
-    assert len(results) == 1
-    assert NO_RESULTS in results[0].text.strip()
+    assert len(search_results) == 1
+    assert any(
+        NO_RESULTS.lower() in result["title"].lower() for result in search_results
+    )
+
+
+def test_successful_search(appium_driver) -> None:
+    explore_page = ExplorePage(appium_driver)
+
+    explore_page.click_button(explore_page.skip_button)
+
+    explore_page.click_button(explore_page.search_container)
+
+    explore_page.fill_input(explore_page.search_input, SEARCH_WORD_VALID)
+
+    search_results = explore_page.get_search_results(
+        explore_page.search_results, explore_page.result_elements
+    )
+
+    assert len(search_results) >= 1
+    assert any(
+        SEARCH_WORD_VALID.lower() in result["title"].lower()
+        for result in search_results
+    )
