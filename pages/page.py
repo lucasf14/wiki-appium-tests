3bd0e80840c2265348e84d887c32ccfccec499a6
Lucas Ferreira <lucassky14@gmail.com>
2026-04-22 11:31:10 +0100

    Adding find child elements function along with first negative test

--- pages/page.py
+++ pages/page.py
@@ -1,16 +1,25 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from appium.webdriver.webdriver import WebDriver
 
 
 class Page:
-    def __init__(self, driver):
+    def __init__(self, driver: WebDriver):
         self.driver = driver
         self.wait = WebDriverWait(self.driver, 10)
 
-    def click_button(self, button_locator):
+    def click_button(self, button_locator: tuple):
         button = self.wait.until(EC.element_to_be_clickable(button_locator))
         button.click()
 
-    def fill_input(self, input_locator, text):
+    def fill_input(self, input_locator: tuple, text: str):
         input_element = self.wait.until(EC.visibility_of_element_located(input_locator))
         input_element.send_keys(text)
+
+    def find_inner_elements(self, parent_locator: tuple, child_locator: tuple) -> None:
+        parent_element = self.wait.until(
+            EC.visibility_of_element_located(parent_locator)
+        )
+
+        child_element = parent_element.find_elements(*child_locator)
+        return child_element
--- tests/test_search.py
+++ tests/test_search.py
@@ -8,13 +8,11 @@ def test_unsuccessful_wiki_search(appium_driver) -> None:
 
     explore_page.click_button(explore_page.search_container)
 
-    explore_page.fill_input(explore_page.search_input, "Invalid123")
+    explore_page.fill_input(explore_page.search_input, "QWERTY123456")
 
     results = explore_page.find_inner_elements(
         explore_page.search_results, explore_page.result_elements
     )
 
-    # assert len(results) == 1
-    # assert "No results" in results[0].text.strip()
-    for result in results:
-        print(f"RESULT: {result.text.strip()}")
+    assert len(results) == 1
+    assert "No results" in results[0].text.strip()
